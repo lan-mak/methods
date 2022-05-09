e3b27f989c35d9d72e792530c30dc083ddcb8214
lan-mak <lan-mak@ya.ru>
2022-05-09 10:16:07 +0300

    ver 0.03

--- src/js/character.js
+++ src/js/character.js
@@ -27,9 +27,8 @@ export default class Character {
   }
 
   damage(points) {
-    if (this.health >= 0) {
-      this.health -= points * (1 - this.defence / 100);
-    }
+    this.health -= points * (1 - this.defence / 100);
+
     if (this.health < 0) {
       this.health = 0;
     }
